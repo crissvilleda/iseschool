2f7a53ff997124b4841f20d80ed099ac539d2d98
crissvilleda <desarrollo01@ciancoders.dev>
2022-03-14 22:39:14 -0600

    ADDED: started for the app iseschool

---
+++ src/pages/home/App.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { db } from "../../firebase";
+import { collection, getDocs } from "firebase/firestore";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    async function loadData() {
+      const querySnapshot = await getDocs(collection(db, "users"));
+      const arrayUsers = [];
+      querySnapshot.forEach((doc) => {
+        arrayUsers.push(doc.data());
+      });
+      setUsers(arrayUsers);
+    }
+    loadData().then();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Ise School Start App</p>
+        <ul>
+          {users.map((item, index) => (
+            <li key={index}>{item.name}</li>
+          ))}
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
