992a0a12e0568c86c9e6358d2b8ba1939878b8e4
crissvilleda <desarrollo01@ciancoders.dev>
2022-03-24 20:13:02 -0600

    ADDED: solved problem in TitleUnderline

--- src/components/TitleUnderline/TitleUnderline.jsx
+++ src/components/TitleUnderline/TitleUnderline.jsx
@@ -1,9 +1,17 @@
 import React, { useEffect } from "react";
 import "./Title.css";
 
-const TitleUnderline = ({ title, color = "#296073", className = "" }) => {
+const uniqueId = () =>
+  Date.now().toString(36) + Math.random().toString(36).substring(2);
+
+const TitleUnderline = ({
+  title,
+  color = "#296073",
+  className = "",
+  id_title = "title-" + uniqueId(),
+}) => {
   const loadTitle = () => {
-    const element = document.querySelector("#title");
+    const element = document.querySelector(`#${id_title}`);
     element.style.setProperty("--ww", "0px");
     const width_add = 30;
     const element_width = element.offsetWidth + width_add;
@@ -17,7 +25,7 @@ const TitleUnderline = ({ title, color = "#296073", className = "" }) => {
     loadTitle();
   }, []);
   return (
-    <h1 className={`title-underline ${className}`} id="title">
+    <h1 className={`title-underline ${className}`} id={id_title}>
       {title}
     </h1>
   );
