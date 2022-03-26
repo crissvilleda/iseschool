82f90a5da289604dd094f459e53285a063e895da
crissvilleda <desarrollo01@ciancoders.dev>
2022-03-26 08:31:12 -0600

    ADDED: fixed scroll in tables

--- src/pages/student/StudentForm.jsx
+++ src/pages/student/StudentForm.jsx
@@ -82,6 +82,50 @@ export default function StudentForm({ onSubmit }) {
           </div>
         </div>
       </div>
+      <TitleUnderline title="Datos del sistema" />
+      <div className="is-flex">
+        <div className="field column is-6">
+          <label htmlFor="test" className="label">
+            Nombre Usuario
+          </label>
+          <InputText
+            className="input"
+            control={control}
+            name="nameUser"
+            rules={{ required: "Este campo es requerido" }}
+            placeholder="Ingrese nombre"
+          />
+        </div>
+
+        <div className="field column is-6">
+          <label htmlFor="test" className="label">
+            Contraseña
+          </label>
+          <InputText
+            className="input"
+            control={control}
+            name="password"
+            rules={{ required: "Este campo es requerido" }}
+            placeholder="Ingrese contraseña"
+            type="password"
+          />
+        </div>
+      </div>
+      <div className="is-flex">
+        <div className="field column is-6" />
+        <div className="field column is-6">
+          <label htmlFor="test" className="label">
+            Correo
+          </label>
+          <InputText
+            className="input"
+            control={control}
+            name="email"
+            rules={{ required: "Este campo es requerido" }}
+            placeholder={"Ingrese Correo"}
+          />
+        </div>
+      </div>
 
       <div className="is-flex is-justify-content-space-between">
         <Link className="button is-secondary " to="/user">
