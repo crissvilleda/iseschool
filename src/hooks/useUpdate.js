e2a63cc8f3be0d5d2357918f7dbc8129459a81f2
Gamcas <josesiitocasti@gmail.com>
2022-04-03 13:13:47 -0600

    arreglo de crud de usuarios

--- src/hooks/useUpdate.js
+++ src/hooks/useUpdate.js
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
+import LoadingContext from "../context/LoadingContext";
 
 import { db } from "../firebase";
 
@@ -12,19 +13,23 @@ export default function useUpdate(
   const { id } = useParams();
   const [data, setData] = useState({});
   const isUpdating = id ? true : false;
+  const {setLoading}= useContext(LoadingContext);
 
   const updateData = async (docData) => {
+    setLoading(true)
     const docRef = doc(db, _collection, id);
     await updateDoc(docRef, docData);
     if (redirect) navigate(redirect);
+    setLoading(false);
   };
 
   useEffect(() => {
     if (id) {
+      setLoading(true);
       const docRef = doc(db, _collection, id);
       getDoc(docRef).then((docSnap) => {
         if (docSnap.exists()) setData(docSnap.data());
-      });
+      }).finally(()=>setLoading(false));
     }
   }, []);
 
--- src/pages/user/UserList.jsx
+++ src/pages/user/UserList.jsx
@@ -1,28 +1,67 @@
-import { useMemo, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import Table from "../../components/Table";
-import { useList } from "../../hooks";
+import { useMemo, useEffect, useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Table, { tableActions } from "../../components/Table";
 import dayjs from "dayjs";
 import UserIcon from "../../assets/img/user.png";
-import{
-  InputSelect,
-} from "../../components/CustomInputs"
+import { Select } from "antd";
+import LoadMask from "../../components/LoadMask";
+import {
+  collection,
+  query,
+  orderBy,
+  startAfter,
+  limit,
+  getDocs,
+  where,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
+import { db } from "../../firebase";
+import LoadingContext from "../../context/LoadingContext.js";
+const { Option } = Select;
 
 export default function () {
-  const { getData } = useList("users", "name");
   const [users, setUsers] = useState([]);
+  const [typeUser, setTypeUser] = useState(null);
+  const navigate = useNavigate();
+  const { loading, setLoading } = useContext(LoadingContext);
 
   useEffect(() => {
-    getData().then((data) => {
-      if (data) setUsers(data);
-    });
+    setLoading(true);
+    getUser().finally(() => setLoading(false));
   }, []);
 
+  async function getUser(filterType = null) {
+    let typeUser = ["Admin", "Teacher"];
+    if (filterType != null) {
+      typeUser = typeUser.filter((value) => value == filterType);
+    }
+
+    let querySet = query(
+      collection(db, "users"),
+      where("type", "in", typeUser),
+      limit(25)
+    );
+    const querySnapshot = await getDocs(querySet);
+    const results = [];
+    querySnapshot.forEach((doc) => results.push({ id: doc.id, ...doc.data() }));
+    setUsers(results);
+  }
+
+  async function removeData(id) {
+    setLoading(true);
+    await deleteDoc(doc(db, "users", id));
+    await getUser(typeUser);
+    setLoading(false);
+  }
   const columns = useMemo(
     () => [
       {
         Header: "Herramientas",
-        accessor: "id",
+        accessor: tableActions({
+          edit: (id) => navigate(`/user/${id}`),
+          remove: (id) => removeData(id),
+        }),
       },
       {
         Header: "Nombres",
@@ -50,45 +89,53 @@ export default function () {
     ],
     []
   );
+  console.log(typeUser);
+  console.log(users);
 
   return (
     <>
       <div className="is-flex is-justify-content-space-between my-4">
         <div className="is-flex">
-          <img src={UserIcon} className="title-icon"/>
+          <img src={UserIcon} className="title-icon" />
           <h1 className="title is-3">Usuarios</h1>
         </div>
         <Link to="/user/create" className="button is-secondary">
           Agregar nuevo
         </Link>
-      
-         <div className="is-flex is-fle">
-          <div className="field column is-6">
-            <label htmlFor="test" className="label">
-              Filtrar por tipo
-            </label>
-            <div className="control">
-              <InputSelect
-                className="input"
-                control={control}
-                name="filter"
-                rules={{required: "Este campo es requerido"}}
-                placeholder="Seleccione tipo"
-                options={[
-                  {value: "Admin", label: "Administrador"},
-                  {value: "Teacher", label: "Catedratico"},
-                ]}
-              />
-            </div>
+      </div>
+
+      <div className="is-flex is-fle">
+        <div className="field column is-6">
+          <label htmlFor="test" className="label">
+            Filtrar por tipo
+          </label>
+          <div className="control">
+            <Select
+              className="input"
+              onChange={(value) => {
+                setTypeUser(value);
+                getUser(value);
+              }}
+              placeholder="Seleccione Tipo"
+              value={typeUser}
+              bordered={false}
+              size="large"
+            >
+              <Option key="Admin" value="Admin">
+                Administrador
+              </Option>
+              <Option key="Teacher" value="Teacher">
+                Catedratico
+              </Option>
+            </Select>
           </div>
         </div>
-        */
-
-
       </div>
       <br />
       <br />
-      <Table columns={columns} data={users} />
+      <LoadMask loading={loading}>
+        <Table columns={columns} data={users} />
+      </LoadMask>
     </>
   );
 }
