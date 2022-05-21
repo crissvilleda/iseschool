eb6cb1cc572286de37ee4920c335ac41d375e89f
crissvilleda <desarrollo01@ciancoders.dev>
2022-05-20 23:21:30 -0600

    ADDED: workbox for pwa

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   return defineConfig({
-    plugins: [react()],
+    plugins: [react(), VitePWA({})],
     build: {
       chunkSizeWarningLimit: 1600,
       rollupOptions: {
