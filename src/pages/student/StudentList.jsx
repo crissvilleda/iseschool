bde32cd079189813a1085a6c506883f5f7ba362c
crissvilleda <desarrollo01@ciancoders.dev>
2022-05-21 14:19:06 -0600

    ADDED: updated gender

--- src/pages/student/StudentList.jsx
+++ src/pages/student/StudentList.jsx
@@ -83,7 +83,7 @@ export default function StudentList() {
       },
       {
         Header: "Genero",
-        accessor: "gender",
+        accessor: (row) => (row.gender === "M" ? "Masculino" : "Femenino"),
       },
     ],
     []
