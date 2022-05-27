f74083b27a8c8f86aa452e67d867a2d137562d62
crissvilleda <desarrollo01@ciancoders.dev>
2022-05-26 20:26:43 -0600

    ADDED: auto update pwa

--- vite.config.js
+++ vite.config.js
@@ -41,6 +41,7 @@ export default ({ mode }) => {
       }),
       react(),
       VitePWA({
+        registerType: "autoUpdate",
         includeAssets: [
           "school.png",
           "school.ico",
